4ec6717470655537befbf8f98fcb867294f29e37
Angelo <angelo.palmieri@rufusleonard.com>
2020-06-29 23:43:28 +0100

    bootstrap - Table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Pagination from 'react-bootstrap/Pagination';
 import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
 import './App.scss';
 
 const url = `http://nyx.vima.ekt.gr:3000/api/books`;
@@ -28,8 +29,15 @@ const App = () => {
       .catch((error) => console.error('Error:', error));
   }, [itemsPerPage, currentPage]);
 
+  const updateQueryString = (page) => {
+    const params = new URLSearchParams(window.location.search);
+
+    params.set('page', page);
+    window.history.replaceState({}, '', `${window.location.pathname}?${params}`)
+  }
   const pageChanged = e => {
     setCurrentPage(parseInt(e.target.text));
+    updateQueryString(parseInt(e.target.text));
   };
 
   const PaginationElement = () => {
@@ -53,12 +61,29 @@ const App = () => {
   }
 
   return (
-    <Container className="App">
+    <Container>
       {list.length > 0 ? (
-        <div>
-          {list.map((book) => <p key={book.id}>{book.book_title}</p>)}
+        <Fragment>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Author(s)</th>
+                <th>Year</th>
+              </tr>
+            </thead>
+            <tbody>
+              {list.map((book) => (
+                <tr key={book.id}>
+                  <td>{book.book_title}</td>
+                  <td>{book.book_author}</td>
+                  <td>{book.book_publication_year}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
           <PaginationElement />
-        </div>
+        </Fragment>
       ) : (
         <p>No book available</p>
       )}
