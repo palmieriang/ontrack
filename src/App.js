be3777a77c5d172bc65e711b03a161a7464e2539
Angelo <angelo.palmieri@rufusleonard.com>
2020-06-29 19:32:37 +0100

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [list, setList] = useState({});
+  console.info('list', list);
+
+  useEffect(() => {
+    const url = 'http://nyx.vima.ekt.gr:3000/api/books';
+    const requestOptions = {
+      method: 'POST',
+    };
+
+    fetch(url, requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        setList(data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
